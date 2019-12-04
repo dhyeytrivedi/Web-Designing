fd3460b791f761f8fbbb41334121294f1738b94b
dhyeytrivedi <dhyey.trivedi96@gmail.com>
2019-12-03 21:56:39 -0500

    Final Website

---
+++ Final Website/finalscripts.js
@@ -0,0 +1,11 @@
+//JS for final2html
+function moreinfo() {
+document.getElementById("newPara").innerHTML="Time in physics is unambiguously operationally defined as what a clock reads. See Units of Time. Time is one of the seven fundamental physical quantities in both the International System of Units and International System of Quantities. Time is used to define other quantities – such as velocity – so defining time in terms of such quantities would result in circularity of definition.";
+}
+//JS for final3html
+function newImage(){
+document.getElementById("carimg").src="https://autowise.com/wp-content/uploads/2019/02/maserati-quattroporte-e1550024421251.jpg";
+}
+function oldImage(){
+document.getElementById("carimg").src="https://media.ed.edmunds-media.com/maserati/ghibli/2019/oem/2019_maserati_ghibli_sedan_s-gransport_fq_oem_2_815.jpg";
+}
